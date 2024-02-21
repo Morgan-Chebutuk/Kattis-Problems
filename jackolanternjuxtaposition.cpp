667c5e4271d4dc1aefe096f8ef29ce36fe58583e
Morgan-Chebutuk <122320890+Morgan-Chebutuk@users.noreply.github.com>
2024-02-20 19:17:58 -0600

    Solved jackolanternjuxtaposition.cpp

---
+++ jackolanternjuxtaposition.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << a * b * c << endl;
+}
